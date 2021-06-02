4ba471259327ce05dddd245c9ebcfa7aa24d920b
Sojith Sunny <contactsojith@gmail.com>
2021-06-02 19:12:28 +0530

    V2

--- kode editor-v2/script.js
+++ kode editor-v2/script.js
@@ -1,13 +1,13 @@
 function run() {
-    let html = document.querySelector(".editor div #html").value;
-    let css = document.querySelector(".editor div #css").value;
-    let js = document.querySelector(".editor div #js").value;
+    let html = document.querySelector(".editor #html").value;
+    let css = document.querySelector(".editor #css").value;
+    let js = document.querySelector(".editor #js").value;
     let output = document.querySelector(".output #output");
 
     output.contentDocument.body.innerHTML = html + "<style>"+css+"</style>";
 }
 
 
-document.querySelector(".editor div #html").addEventListener("keyup",run);
-document.querySelector(".editor div #css").addEventListener("keyup",run);
-document.querySelector(".editor div #js").addEventListener("keyup",run);
+document.querySelector(".editor #html").addEventListener("keyup",run);
+document.querySelector(".editor #css").addEventListener("keyup",run);
+document.querySelector(".editor #js").addEventListener("keyup",run);
